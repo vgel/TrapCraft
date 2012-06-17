bb254ec859fdde1cde3d50d990b3aeda8cad6c0f
Jonathon Vogel <jonathon.vogel@gmail.com>
2012-06-16 20:57:42 -0400

    FINALLY

---
+++ trapcraft/BlockObsidianPressurePlate.java
@@ -0,0 +1,13 @@
+package net.minecraft.trapcraft;
+
+import net.minecraft.src.Block;
+import net.minecraft.src.Material;
+
+public class BlockObsidianPressurePlate extends Block {
+
+	protected BlockObsidianPressurePlate(int id, int tex, Material material) {
+		super(id, tex, material);
+		
+	}
+	
+}
